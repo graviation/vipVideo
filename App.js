20dcbb69a96db47919304c4c4d13d0688cb0aaaf
liuy <liuyang_06_01@163.com>
2019-11-07 14:11:55 +0800

    + 添加视频网站

--- App.js
+++ App.js
@@ -1,11 +1,18 @@
 import React from 'react';
-import {View, Button, StatusBar} from 'react-native';
+import {
+  View,
+  Button,
+  StatusBar,
+  Image,
+  Dimensions,
+  TouchableOpacity,
+} from 'react-native';
 import {createAppContainer} from 'react-navigation';
 import {createStackNavigator} from 'react-navigation-stack';
 import Details from './Details';
-import {exp} from 'react-native-reanimated';
 import Play from './Play';
 
+const {width, heigth} = Dimensions.get('window');
 class VideoList extends React.Component {
   static navigationOptions = {
     header: null,
@@ -13,16 +20,56 @@ class VideoList extends React.Component {
 
   render() {
     return (
-      <View style={{flex: 1}}>
+      <View
+        style={{
+          flex: 1,
+          marginTop: 10,
+          flexDirection: 'row',
+          justifyContent: 'space-around',
+        }}>
         <StatusBar hidden={true} translucent={true} />
-        <Button
-          title={'打开爱奇艺'}
+        {/*爱奇艺视频*/}
+        <TouchableOpacity
+          style={{height: width / 5}}
+          activeOpacity={0.8}
           onPress={() =>
             this.props.navigation.navigate('Details', {
               url: 'https://m.iqiyi.com/',
             })
-          }
-        />
+          }>
+          <Image
+            source={require('./img/classify/aqiyi.jpg')}
+            style={{width: width / 5, height: width / 5, borderRadius: 10}}
+          />
+        </TouchableOpacity>
+        {/*腾讯视频*/}
+        <TouchableOpacity
+          style={{height: width / 5}}
+          activeOpacity={0.8}
+          onPress={() =>
+            this.props.navigation.navigate('Details', {
+              url: 'https://m.v.qq.com/',
+            })
+          }>
+          <Image
+            source={require('./img/classify/tengxun.jpg')}
+            style={{width: width / 5, height: width / 5, borderRadius: 10}}
+          />
+        </TouchableOpacity>
+        {/*优酷视频*/}
+        <TouchableOpacity
+          style={{height: width / 5}}
+          activeOpacity={0.8}
+          onPress={() =>
+            this.props.navigation.navigate('Details', {
+              url: 'https://www.youku.com/',
+            })
+          }>
+          <Image
+            source={require('./img/classify/youku.jpg')}
+            style={{width: width / 5, height: width / 5, borderRadius: 10}}
+          />
+        </TouchableOpacity>
       </View>
     );
   }
--- Details.js
+++ Details.js
@@ -19,14 +19,24 @@ export default class Details extends React.Component {
           source={{uri: this.props.navigation.getParam('url')}}
           onNavigationStateChange={event => {
             const url = event.url;
-            const iqiyi = 'm.iqiyi.com/.*html';
-            if (url.match(iqiyi) && url.match(iqiyi).length > 0) {
+            const iqiyi = 'm.iqiyi.com/.*html'; // 爱奇艺视频播放链接正则
+            const tengxun = 'm.v.qq.com/.*html'; // 腾讯视频播放链接正则
+            const youku = 'm.youku.com/alipay_video/id.*html'; // 优酷视频播放链接正则
+            this.setState({videoUrl: url});
+            if (
+              (url.match(iqiyi) && url.match(iqiyi).length > 0) ||
+              (url.match(tengxun) && url.match(tengxun).length > 0) ||
+              (url.match(youku) && url.match(youku).length > 0)
+            ) {
               this.setState({videoUrl: url, showBtn: true});
+            } else {
+              this.setState({showBtn: false});
             }
           }}
         />
         {this.state.showBtn ? (
           <Button
+            color={'orange'}
             title={'VIP播放'}
             onPress={() =>
               this.props.navigation.navigate('Play', {
@@ -35,6 +45,7 @@ export default class Details extends React.Component {
             }
           />
         ) : null}
+        {/*<Text>{this.state.videoUrl}</Text>*/}
       </View>
     );
   }
